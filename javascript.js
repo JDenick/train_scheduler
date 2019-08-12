d4b413975d3a83da37b1f8e03964ac6a5148450e
Jesse Denick <jessedenick@gmail.com>
2019-08-12 13:28:43 -0400

    initialized firebse and successfully appended new rows to html

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
 alert ("Resetting..")
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDTTN_C3nKl6xhX-Wp0UrDBZ_d5gdIFUnE",
+    authDomain: "train-scheduler-efe5d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-efe5d.firebaseio.com",
+    projectId: "train-scheduler-efe5d",
+    storageBucket: "",
+    messagingSenderId: "583511190220",
+    appId: "1:583511190220:web:13c9cb5ee6316deb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
 //button for adding employees
 $("#submit-btn").on("click", function(event){
     event.preventDefault()
@@ -17,7 +31,7 @@ $("#submit-btn").on("click", function(event){
         frequency: trainFrequency
     };
 
-    //database.ref().push(newTrain);
+    database.ref().push(newTrain);
 
     console.log(newTrain.name);
     console.log(newTrain.destination);
@@ -30,4 +44,27 @@ $("#submit-btn").on("click", function(event){
     $("#destination-input").val("");
     $("#time-input").val("");
     $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(trainTime),
+        $("<td>").text(trainFrequency)
+    );
+
+    $("#train-table > tbody").append(newRow)
 });
